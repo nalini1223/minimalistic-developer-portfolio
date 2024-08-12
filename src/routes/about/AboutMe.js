8fb76e36d45fef8f068b78ca4d98ed0c36cac0e8
Nalini Uday Timmaraju <127900389+nalini1223@users.noreply.github.com>
2024-08-11 23:28:59 -0500

    Update AboutMe.js
    Personal

--- src/routes/about/AboutMe.js
+++ src/routes/about/AboutMe.js
@@ -3,7 +3,7 @@ const AboutMe = () => {
     <section>
       <h2 className="title-font pink-text h2-tag">About Me</h2>
       <p className="white-text p-tag">
-      I am seasoned Senior DevOps Engineer with over 5 years of experience in sectors like Telecom and E-commerce. She has led critical projects, including the migration of WebLogic applications to Kubernetes at T-Mobile, where she demonstrated expertise in containerization, orchestration, and automation using tools like Ansible and Terraform. Nalini has also collaborated on integrating SlackOps and has worked on significant DevOps and cloud projects at Google and Amazon, further honing her skills in CI/CD pipelines, Infrastructure as Code, and cloud technologies.      </p>
+       I am seasoned Senior DevOps Engineer with over 5 years of experience in sectors like Telecom and E-commerce. She has led critical projects, including the migration of WebLogic applications to Kubernetes at T-Mobile, where she demonstrated expertise in containerization, orchestration, and automation using tools like Ansible and Terraform. Nalini has also collaborated on integrating SlackOps and has worked on significant DevOps and cloud projects at Google and Amazon, further honing her skills in CI/CD pipelines, Infrastructure as Code, and cloud technologies.      </p>
     </section>
   );
 };
