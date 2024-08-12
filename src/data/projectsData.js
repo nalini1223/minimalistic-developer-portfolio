88add5850e273636e8fbea0f77e1a06bb8f3dd9b
Nalini Uday Timmaraju <127900389+nalini1223@users.noreply.github.com>
2024-08-11 22:41:11 -0500

    Personal Protofolio

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -10,7 +10,7 @@ const projectsData = [
     name: "Rest Countires",
     stack: ["< RESTful API />", "< CSS3 />", "< React.js />"],
     src: "https://restful-countries.vercel.app/",
-    source: "https://github.com/catherineisonline/rest-countries",
+    source: "https://github.com/nalini1223/rest-countries",
     description:
       "REST Countries is a project made with API which also has a color theme switcher feature. You can view the list of all countries and find a specific one via search or by filtering regions. By navigating to the individual country page, you are able to see more details about the country like native name and even border countries.",
   },
@@ -23,16 +23,6 @@ const projectsData = [
     source: "https://github.com/nalini1223/Arduino-Morse-Code-Translator",
     description:
       "In Morse code, each number and letter of the alphabet are represented by a series of dots and dashes. Create your own message in morse code using only one LED and an Arduino board. Morse Code Day (27th of April) pays tribute to this traditional form of communication that has been in practice for nearly 160 years. It involves using standardized sequences of two different signal durations: dots and dashes. It was named after Samuel F. B. Morse, that contributed to the telegraph invention.",
-  },
-  {
-    id: "bankist",
-    img: Bankist,
-    name: "Bankist",
-    stack: ["< Vanilla JS />", "< CSS3 />", "< HTML5 />"],
-    src: "https://bankist-bank.vercel.app/",
-    source: "https://github.com/catherineisonline/bankist",
-    description:
-      "Bankist is a fictional online bank and its goal is to practice DOM manipulation and user interaction. Bankist has an option to log in to the account and use different functionalities, almost like in an actual bank! By logging into your account you can transfer money to another account, take a loan or delete the account. Please check README for login info.",
   },
 ];
 
--- src/routes/about/AboutMe.js
+++ src/routes/about/AboutMe.js
@@ -3,7 +3,7 @@ const AboutMe = () => {
     <section>
       <h2 className="title-font pink-text h2-tag">About Me</h2>
       <p className="white-text p-tag">
-      have 5+ years of experience as a  Senior DevOps engineer working in different sectors of business like Telecom, and E-commerce and my background in managing complex DevOps projects, automating processes, and collaborating effectively across teams makes me confident in my ability to contribute effectively to these projects and initiatives.
+      I have 5+ years of experience as a  Senior DevOps engineer working in different sectors of business like Telecom, and E-commerce and my background in managing complex DevOps projects, automating processes, and collaborating effectively across teams makes me confident in my ability to contribute effectively to these projects and initiatives.
       </p>
     </section>
   );
