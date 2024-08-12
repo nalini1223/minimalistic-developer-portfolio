476fba4f7edcddbaa10a9c563dd0ac22feeeb173
Nalini Uday Timmaraju <127900389+nalini1223@users.noreply.github.com>
2024-08-11 22:14:37 -0500

    Personal Portfolio

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -4,10 +4,10 @@ const Footer = () => {
   return (
     <article className="footer">
       <a
-        href="https://github.com/catherineisonline/"
+        href="https://github.com/nalini1223"
         target="_blank"
         rel="noreferrer">
-        Designed & Built by Sophia &copy;2023
+        Designed & Built by Nalini &copy;2023
       </a>
     </article>
   );
--- src/components/navigation/NavigationSocials.js
+++ src/components/navigation/NavigationSocials.js
@@ -5,21 +5,21 @@ const NavigationSocials = () => {
   return (
     <section className="social-media">
       <a
-        href="https://www.linkedin.com/in/catherinemitagvaria/"
+        href="https://www.linkedin.com/in/nalini-t-0950a4219/"
         target="_blank"
         rel="noreferrer"
       >
         <img src={Linkedin} alt="linkedin" />
       </a>
       <a
-        href="https://github.com/catherineisonline"
+        href="https://github.com/nalini1223"
         target="_blank"
         rel="noreferrer"
       >
         <img src={Github} alt="github" />
       </a>
       <a
-        href="https://codepen.io/catherineisonline"
+        href="https://codepen.io/nalini1223"
         target="_blank"
         rel="noreferrer"
       >
